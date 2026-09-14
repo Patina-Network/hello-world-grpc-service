ce268ce89ba7f39461f5a9b99e4f4b6c9dcf2fd9
Tahmid Ahmed <tahmid@tahmid.io>
2026-09-14 12:41:30 -0400

    TAN-980: add CI, add tests to CI

---
+++ exclusions.ts
@@ -0,0 +1,6 @@
+/**
+ * See https://github.com/Patina-Network/hello-world-grpc-service/blob/main/.github/scripts/src/test/index.ts
+ * for test exclusion usages.
+ */
+
+export const exclusions = ["**/*.rs"] as const;
