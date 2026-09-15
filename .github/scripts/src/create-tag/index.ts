af9b34ff4d7f50770327066b1ef7db6680eb8b2e
Tahmid Ahmed <tahmid@tahmid.io>
2026-09-15 14:22:51 -0700

    TAN-980: fix github creds for createTag (#16)
    * TAN-980: fix github creds for createTag
    
    * TAN-980: fix incorrect env key & fix build gRPC server image step title

--- .github/scripts/src/create-tag/index.ts
+++ .github/scripts/src/create-tag/index.ts
@@ -41,25 +41,25 @@ export async function main() {
 
 function parseCiEnv(ciEnv: Record<string, string>) {
   const githubAppAppId = (() => {
-    const v = ciEnv["GITHUB_APP_APP_ID"];
+    const v = ciEnv["_GITHUB_APP_APP_ID"];
     if (!v) {
-      throw new Error("Missing GITHUB_APP_APP_ID from .env.ci");
+      throw new Error("Missing _GITHUB_APP_APP_ID from env");
     }
     return v;
   })();
 
   const githubAppInstallationId = (() => {
-    const v = ciEnv["GITHUB_APP_INSTALLATION_ID"];
+    const v = ciEnv["_GITHUB_APP_INSTALLATION_ID"];
     if (!v) {
-      throw new Error("Missing GITHUB_APP_INSTALLATION_ID from .env.ci");
+      throw new Error("Missing _GITHUB_APP_INSTALLATION_ID from env");
     }
     return v;
   })();
 
   const githubAppPrivateKey = (() => {
-    const v = ciEnv["GITHUB_APP_PEM_CONTENT"];
+    const v = ciEnv["_GITHUB_APP_PEM_CONTENT"];
     if (!v) {
-      throw new Error("Missing GITHUB_APP_PRIVATE_KEY from .env.ci");
+      throw new Error("Missing _GITHUB_APP_PRIVATE_KEY from env");
     }
     return v;
   })();
